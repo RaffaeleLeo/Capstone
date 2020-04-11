7827548e42594a488892f01ca5642385b5acccaf
Kelton White <whitekelton@yahoo.com>
2020-04-11 00:35:18 -0600

    Weather data is packaged in a class now.

--- app/src/test/java/com/example/avi/ExampleUnitTest.java
+++ app/src/test/java/com/example/avi/ExampleUnitTest.java
@@ -2,6 +2,9 @@ package com.example.avi;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -18,7 +21,10 @@ public class ExampleUnitTest {
     public void WeatherDataTest(){
         //String.valueOf(40.628178), String.valueOf(-111.623462) mountain test coordinates
         WeatherData data = new WeatherData();
-        data.getWeatherData();
+        //Gets the most recent weather data
+        WeatherData.Weather weather = (WeatherData.Weather) data.getWeatherData().get(0);
+        System.out.println(weather.getWindDirection());
+
 
     }
 }
