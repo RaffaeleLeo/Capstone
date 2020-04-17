f330a579a27c282a6c058d62c4fe14689a1ec2fb
Nathan <nathan.zaltsman@gmail.com>
2020-04-17 00:45:03 -0600

    Added basic email/password login Firebase integration

--- app/src/main/java/com/example/avi/LoginActivity.java
+++ app/src/main/java/com/example/avi/LoginActivity.java
@@ -10,12 +10,29 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
    Button loginButton, createButton;
    EditText createFirst, createLast, createEmail, createPassword, loginEmail, loginPassword;
+
+   private FirebaseAuth mAuth;
 
 
     @Override
@@ -34,73 +51,95 @@ public class LoginActivity extends AppCompatActivity {
         createButton = (Button)findViewById(R.id.CreateButton);
 
         final MyDBHandler dbHandler = new MyDBHandler(getApplicationContext(), "users.db", null, 1);
+        mAuth = FirebaseAuth.getInstance();
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = loginEmail.getText().toString().toLowerCase();
-                String password = loginPassword.getText().toString();
+        if(mAuth.getCurrentUser() != null){
+            Toast.makeText(getApplicationContext(), "You are signed in!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, LiveUpdates.class);
+            startActivity(intent);
+        }
 
-                String actualPassword = dbHandler.findUser(email, password);
+        else {
 
-                if(email.equals("") || password.equals("")){
-                    Toast.makeText(getApplicationContext(), "All fields must be filled out.", Toast.LENGTH_LONG).show();
-                }
 
-                else{
-                    if (password.equals(actualPassword)) {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String email = loginEmail.getText().toString().toLowerCase();
+                    String password = loginPassword.getText().toString();
 
-                        Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, LiveUpdates.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getApplicationContext(), "User not found, please try again", Toast.LENGTH_LONG).show();
-                    }
-                }
-
-            }
-        });
-
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String first = createFirst.getText().toString();
-                String last = createLast.getText().toString();
-                String email = createEmail.getText().toString().toLowerCase();
-                String password = createPassword.getText().toString();
-
-                if(first.equals("") || last.equals("") || email.equals("") || password.equals("")){
-                    Toast.makeText(getApplicationContext(), "All fields must be filled out.", Toast.LENGTH_LONG).show();
-                }
-
-                else if (password.length() < 6){
-                    Toast.makeText(getApplicationContext(), "Password must be at least 6 characters long.", Toast.LENGTH_LONG).show();
-                }
-
-                else {
+                    String actualPassword = dbHandler.findUser(email, password);
 
                     try {
 
-                        dbHandler.addToUsers(email, first, last, password, true);
-                        Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, LiveUpdates.class);
-                        startActivity(intent);
+                        if (email.equals("") || password.equals("")) {
+                            Toast.makeText(getApplicationContext(), "All fields must be filled out.", Toast.LENGTH_LONG).show();
+                        } else {
+                            if (password.equals(actualPassword)) {
 
+                                loginUser(email, password);
+                                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, LiveUpdates.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), "User not found, please try again", Toast.LENGTH_LONG).show();
+                            }
+                        }
 
                     } catch (SQLException e) {
-                        Toast.makeText(getApplicationContext(), "An account with this email already exists, please login using your created account.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "User not found, please try again", Toast.LENGTH_LONG).show();
                     }
 
                 }
+            });
+
+            createButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String first = createFirst.getText().toString();
+                    String last = createLast.getText().toString();
+                    String email = createEmail.getText().toString().toLowerCase();
+                    String password = createPassword.getText().toString();
+
+                    if (first.equals("") || last.equals("") || email.equals("") || password.equals("")) {
+                        Toast.makeText(getApplicationContext(), "All fields must be filled out.", Toast.LENGTH_LONG).show();
+                    } else if (password.length() < 6) {
+                        Toast.makeText(getApplicationContext(), "Password must be at least 6 characters long.", Toast.LENGTH_LONG).show();
+                    } else {
+
+                        try {
+                            dbHandler.addToUsers(email, first, last, password, true);
+
+                            registerUser(email, password);
+
+                            Toast.makeText(getApplicationContext(), "Welcome!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, LiveUpdates.class);
+                            startActivity(intent);
 
 
+                        } catch (SQLException e) {
+                            Toast.makeText(getApplicationContext(), "An account with this email already exists, please login using your created account.", Toast.LENGTH_LONG).show();
+                        }
 
-            }
-        });
+
+                    }
 
 
-        //setupTabLayout();
+                }
+            });
+
+
+            //setupTabLayout();
+        }
     }
+
+    //TODO: Put all button/login logic in here. 
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+
 
 
 
@@ -142,4 +181,33 @@ public class LoginActivity extends AppCompatActivity {
 
         tabLayout.getTabAt(1).select();
     }*/
+
+    private void registerUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            throw new SQLException();
+                        }
+                    }
+                });
+    }
+
+    private void loginUser(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            throw new SQLException();
+                        }
+
+                    }
+                });
+    }
 }
